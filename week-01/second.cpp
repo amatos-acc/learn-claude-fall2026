8515616f373d8650cf1a0f929774d7b82f72e0ef
Alexander Matos <alexander01331412@gmail.com>
2026-06-25 18:14:25 -0500

    Add variables example

---
+++ week-01/second.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	int age = 19;
+	double gpa = 3.8; 
+	std::string major = "Computer Science";
+
+	std::cout << "Age: " << age <<  std::endl;
+	std::cout << "GPA: " << gpa <<  std::endl;
+	std::cout << "Major: " << major <<  std::endl;
+
+	return 0;
+}
