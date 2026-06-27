56e4f9ebffe461f1bc1b507b4f76d3860199e96b
Alexander Matos <alexander01331412@gmail.com>
2026-06-27 17:05:05 -0500

    Add for and while loop examples

---
+++ week-01/loops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	for (int i = 1; i <= 10; i++) {
+		cout << "Current number is: " << i << "\n";
+	}
+
+	int j = 10;
+
+	while (j >= 1) {
+		cout << "Current number is: " << j << "\n";
+		j--;
+	}
+
+	return 0;
+}
